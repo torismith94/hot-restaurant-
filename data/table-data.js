ea8d62a0cdc1c193ca013f26a13ce1769985e68e
Victoria Smith <torismith@Victorias-MacBook-Pro-3.local>
2017-11-18 13:16:13 -0800

    JS added to html

---
+++ data/table-data.js
@@ -0,0 +1,10 @@
+var tableArray = [
+{
+	customerName: "Ryan Howard",
+	customerEmail: "ryan@example.com",
+	customerId: "ryanhoward",
+	phoneNumber: "000-000-0000"
+}
+];
+
+module.exports = tableArray;
