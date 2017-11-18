b1e54b5dada736ffc657f36246928e59fbeb1582
Ryan Howard <ryanhoward810@gmail.com>
2017-11-18 13:00:34 -0800

    committing data and route files

---
+++ data/waitinglist-data.js
@@ -0,0 +1,10 @@
+var waitingArray = [
+{
+	customerName: "Evan Campbell",
+	customerEmail: "evan@example.com",
+	customerId: "evancampbell",
+	phoneNumber: "000-000-0000"
+}
+];
+
+module.exports = waitingArray;
