89da57611c39af200a369a3287d76cbf751f7d33
Ryan Howard <ryanhoward810@gmail.com>
2017-11-18 13:12:39 -0800

    committing revised html-routes.js

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -3,11 +3,11 @@ var path = require('path');
 module.exports = function (app) {
 
 	app.get('/tables', function(req, res) {
-  		res.sendFile(path.join(__dirname + '/../public/tables.html'));
+  		res.sendFile(path.join(__dirname + '/../tables.html'));
 });
 	app.get('/reserve', function(req, res) {
-  		res.sendFile(path.join(__dirname + '/../public/reserve.html'));
+  		res.sendFile(path.join(__dirname + '/../reserve.html'));
 
   	app.use('/', function(req, res) {
-  		res.sendFile(path.join(__dirname + '/../public/home.html'));	
+  		res.sendFile(path.join(__dirname + '/../home.html'));	
 }
