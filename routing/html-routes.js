f702fec509fe45685873d1ebc9b8f6ee3909a684
paulthorman <30275453+paulthorman@users.noreply.github.com>
2017-11-18 13:31:14 -0800

    Update html-routes.js

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -3,21 +3,14 @@ var path = require('path');
 module.exports = function (app) {
 
 	app.get('/tables', function(req, res) {
-<<<<<<< HEAD
-  		res.sendFile(path.join(__dirname + '/../public/tables.html'));
-});
-	app.get('/reserve', function(req, res) {
-  		res.sendFile(path.join(__dirname + '/../public/reserve.html'));
-
-  	app.use('/', function(req, res) {
-  		res.sendFile(path.join(__dirname + '/../public/home.html'));	
-=======
   		res.sendFile(path.join(__dirname + '/../tables.html'));
-});
+	});
+
 	app.get('/reserve', function(req, res) {
   		res.sendFile(path.join(__dirname + '/../reserve.html'));
+  	});
 
   	app.use('/', function(req, res) {
-  		res.sendFile(path.join(__dirname + '/../home.html'));	
->>>>>>> fe2005fcb84f23923ef3edcee1737abc6ba0cce8
+  		res.sendFile(path.join(__dirname + '/../home.html'));
+  	});
 }
